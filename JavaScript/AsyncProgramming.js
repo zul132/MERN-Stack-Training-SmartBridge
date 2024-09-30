479fe125d6d8df0800d43e3aee9c5471c7514e06
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-09-30 12:32:22 +0530

    started JS Day 3 - Asynch programming

---
+++ JavaScript/AsyncProgramming.js
@@ -0,0 +1,7 @@
+// ASYNCHRONOUS PROGRAMMING
+/* 
+    Callback functions, promises, and async/await are some of the common asynchronous programming techniques used in JavaScript.
+*/
+
+const p = new Promise( (resolve, reject) => {reject()})
+
