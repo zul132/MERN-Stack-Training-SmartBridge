ef9915ec0192052387bf0ac92b848126fce454fb
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-11-18 10:42:10 +0530

    Updated user Controller and Routes

--- demoAPP/Server/controller/userController.js
+++ demoAPP/Server/controller/userController.js
@@ -1,10 +1,24 @@
 const User = require('../model/user')
 
+
+// GET method to fetch all the users
 exports.getAllUsers = async (req,res)=>{
     try {
         const users = await User.find()
         res.status(200).json(users) 
     } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+
+// POST method to create a new user
+exports.createUser = async (req,res)=>{
+    try {
+        const user = new User(req.body);
+        await user.save();
+
+    } catch(error) {
         res.status(500).json({message:error.message})
     }
 }
--- demoAPP/Server/route/userRoutes.js
+++ demoAPP/Server/route/userRoutes.js
@@ -3,5 +3,10 @@ const router = express.Router()
 const UserController = require('../controller/userController')
 
 router.get('/users', UserController.getAllUsers)
+router.post('/users', UserController.createUser)
+router.put('/users/:id', UserController.updateUser)
+router.delete('/users/:id', UserController.deleteUser)
+router.get('/users/:id', UserController.getUserById)
+
 
 module.exports = router
